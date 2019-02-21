a467ac85e15d0e99f29ef4b434be75155ddc0a80
programer10 <programer10@ucione.local>
2019-02-21 16:36:09 +0000

    dodan prvi ispis texta

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -19,7 +19,12 @@ namespace ConsoleApp1
             //Probna p = new Probna();
             //Console.WriteLine(p.A);
             //Console.WriteLine("Rješenje je zbroj 20 + 10 = "+ p.Add(p.A,p.b));
-            Console.WriteLine("Ovo je moj prvi program");
+            Console.WriteLine("Ovo je moj " +
+                " današnji " + 1 +
+                "." +
+                " " +
+                "" +
+                "program");
             //Console.ReadKey();
             Console.ReadLine();
         }
