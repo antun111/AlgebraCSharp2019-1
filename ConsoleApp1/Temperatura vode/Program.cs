fd0bd6125c0df53361fbd26fe57ade693e2cca07
programer10 <programer10@ucione.local>
2019-02-21 18:00:25 +0000

    Zbirka

---
+++ ConsoleApp1/Temperatura vode/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperatura_vode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("0°C");
+            Console.WriteLine("100°C");
+            Console.ReadLine();
+        }
+    }
+}
