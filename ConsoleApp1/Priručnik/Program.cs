2d2678ec4104fa09284bc01a18005ebe1f9fb56d
antun111 <tuna.gfos@gmail.com>
2019-02-25 15:38:29 +0000

    primjer uvj gran

---
+++ ConsoleApp1/Priručnik/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priručnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite prirodan broj");
+            int a = int.Parse(Console.ReadLine());
+            if (a > 10)
+            {
+                Console.WriteLine("Unesi broj je veći od 10");
+            }
+            { Console.WriteLine("Uneseni broj NIJE veći od 10");
+            }
+            Console.ReadKey();
+        }
+    }
+}
