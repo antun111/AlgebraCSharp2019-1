e6b9db7160a5b50429a8e7da9b8aa2714f43da47
Antun Majdančić <tuna.gfos@gmail.com>
2019-02-26 17:37:27 +0000

    suma

---
+++ ConsoleApp1/Suma_prvih_100_brojeva/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma_prvih_100_brojeva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Suma prvih 100
+            Console.WriteLine("\n\nSuma prvih 100");
+            int suma = 0;
+            for (int i =0; i < 5; i++)
+            {
+                Console.Write("{0} ", suma += i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
