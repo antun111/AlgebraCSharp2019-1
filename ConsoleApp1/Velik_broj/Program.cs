4173f08efe5085b6b348bb4ec910378ed75a9a81
Antun Majdančić <tuna.gfos@gmail.com>
2019-02-26 15:30:10 +0000

    brojevi

---
+++ ConsoleApp1/Velik_broj/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Velik_broj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj:");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a > 999)
+            {
+                Console.WriteLine("Broj je velik.");
+            }
+            else
+            {
+                Console.WriteLine("Broj nije velik.");
+            }
+            Console.ReadKey();
+          
+            
+        }
+    }
+}
---
+++ ConsoleApp1/X-znamenkasti_broj/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X_znamenkasti_broj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj:");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a < 0) a = -a;4
+
+            if (a < 10)
+            {
+                Console.WriteLine("Uneseni broj je jednoznamenkast.");
+            }
+            else if (a < 100)
+            {
+                Console.WriteLine("Uneseni broj je dvoznamenkast.");
+            }
+            else if (a < 1000)
+            {
+                Console.WriteLine("Uneseni broj je troznamenkast.");
+            }
+            else
+            {
+                Console.WriteLine("Uneseni broj je višeznamenkast.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
