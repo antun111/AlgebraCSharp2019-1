873a5ddc80f707275810728c17bb1e0567a49fda
Antun Majdančić <tuna.gfos@gmail.com>
2019-03-06 17:44:47 +0000

    Objekti

---
+++ ConsoleApp1/8.1.2._Prirucnik/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._1._2._Prirucnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciraj novi objekt klase Ucenik
+            Ucenik uc1 = new Ucenik();
+
+            //Inicijalizacija objekta
+            Console.WriteLine("Unesite ime učenika: ");
+            uc1.Ime = Console.ReadLine();
+            Console.WriteLine("Unesite prezime učenika: ");
+            uc1.Prezime = Console.ReadLine();
+            Console.Write("Unesite ocjenu iz matematike: ");
+            uc1.OcjenaIzMatematike = int.Parse(Console.ReadLine());
+            Console.Write("Unesite ocjenu iz engleskog: ");
+            uc1.OcjenaIzEngleskog = int.Parse(Console.ReadLine());
+            Console.Write("Unesite ocjenu iz biologije: ");
+            uc1.OcjenaIzBiologije = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Prosjek je: " + uc1.Prosjek());
+
+            //Poziv metode objekta
+            Console.WriteLine("Prosjek je " + uc1.Prosjek() + ".");
+
+            //2. objekt instanciran je pomoću 2. konstruktora
+            Ucenik uc2 = new Ucenik("Perica");
+            Console.WriteLine(uc2.Prezime + " " + uc2.Ime);
+
+            Console.WriteLine("Ime učenika je " + uc1.Ime + " i " + uc2.Ime);
+
+            //List<Ucenik> ucenici = new List<Ucenik>(); // moze i ovako
+            List<Ucenik> ucenici = new List<Ucenik> { uc1, uc2 };
+            ucenici.Add(uc1);
+            ucenici.Add(uc2);
+            foreach (var item in ucenici)
+            {
+                Console.WriteLine(item.Ime);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/8.1.2._Prirucnik/Ucenik.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._1._2._Prirucnik
+{
+    class Ucenik
+    {
+        public System.DateTime DatumNastanka;
+        public string Ime = "";
+        public string Prezime = "";
+        public int OcjenaIzMatematike = 0;
+        public int OcjenaIzEngleskog = 0;
+        public int OcjenaIzBiologije = 0;
+
+        public Ucenik()
+        {
+            this.DatumNastanka = DateTime.Now;
+        }
+
+        public Ucenik(string v)
+        {
+            this.Ime = v;
+        }
+        public Ucenik(string v, string prez)
+        {
+            this.Ime = v;
+            this.Prezime = prez;
+        }
+
+        internal double Prosjek()
+        {
+            double prosjek = 0;
+            prosjek = (double)(OcjenaIzMatematike + OcjenaIzEngleskog + OcjenaIzBiologije) / 3;
+
+            return prosjek;
+            //throw new NotOmplementedException();
+        }
+        public override string ToString()
+        {
+            return "Moje ime je " + this.Ime + " a moj prosjek je: " + this.Prosjek();
+        }
+    }
+}
