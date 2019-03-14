e1a06903215d1ae02aed86589a08ae92a00bd14a
Antun Majdančić <tuna.gfos@gmail.com>
2019-03-14 16:49:53 +0100

    datoteke

---
+++ ConsoleApp1/10.1.1_datoteke/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._1._1_datoteke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Upiši tekst: ");
+            string upisano = Console.ReadLine();
+
+            //Kreiranje handle-a
+            FileStream fs1 = new FileStream("Proba.txt", FileMode.Create);
+
+            //Zapisivanje u datoteku
+            StreamWriter sw = new StreamWriter(fs1);
+
+            sw.WriteLine(upisano);
+
+            sw.Flush();
+
+            sw.Close();
+            fs1.Close();
+
+            //Čitanje datoteke
+            FileStream fs2 = new FileStream("Proba.txt", FileMode.Open);
+
+            StreamReader sr = new StreamReader(fs2);
+
+            string procitano = sr.ReadToEnd();          
+
+            sr.Close();
+            fs2.Close();
+
+            Console.WriteLine(procitano);
+            Console.ReadKey();
+        }
+    }
+}
