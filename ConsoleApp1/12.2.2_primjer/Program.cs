6f7d4bd04727fced0946b703e1314bac174eb2ae
Antun Majdančić <tuna.gfos@gmail.com>
2019-03-15 17:25:57 +0100

    datumi, enumeracije

---
+++ ConsoleApp1/12.2.2_primjer/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._2._2_primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite podatak tipa DateTime: ");
+            DateTime d1 = DateTime.Parse(Console.ReadLine());
+
+            //Ispisujemo datume
+            Console.WriteLine("Datum: ");
+            Console.WriteLine(d1);
+            Console.WriteLine(d1.ToLongDateString());
+            Console.WriteLine(d1.ToShortDateString());
+
+            //Ispisujemo vrijeme
+            Console.WriteLine("Vrijeme: ");
+            Console.WriteLine(d1.ToShortDateString());
+
+            //Ispis dana u tjednu ( 0 = nedjelja )
+            Console.WriteLine("Dan u tjednu: ");
+            // Ako ne "kastamo", ispisuju se slovima
+            Console.WriteLine(d1.DayOfWeek);
+            Console.WriteLine((int)d1.DayOfWeek);
+
+            //Korisničko formatiranje
+            //Sistemsko vrijeme ispisujemo na preciznost
+            //tisućinke sekunde
+            Console.WriteLine("Sada je točno: ");
+            d1 = DateTime.Now;
+
+            Console.WriteLine(d1.ToString("MMMM, dddd yyyy HH:mm:ss fff"));
+
+            //Koliko dana ima do nove godina
+            //1) Kreiramo novi datum za Novu Godinu
+            DateTime dNG = new DateTime(2010, 1, 1);
+
+            //2) Oduzimamo tekući datum od datuma Nove Godine
+            TimeSpan ts = dNG.Subtract(d1);
+
+            //3) Ispisujemo koliko ima do Nove Godine
+            Console.WriteLine("Do Nove Godine ima: ");
+            Console.WriteLine("dana: " + ts.Days);
+            Console.WriteLine(" i sati: " + ts.Hours);
+            Console.WriteLine("To je ukupno sati: " + ts.TotalHours);
+            Console.WriteLine("Ili {0} dana, {1} sati, {2} minuta i {3} sekundi.", ts.Days, ts.Hours, ts.Minutes, ts.Seconds);
+
+            //Povećanje datuma
+            Console.WriteLine("Danas je izdana potvrda na 6 mjeseci.");
+            DateTime DatumIzdavanja = DateTime.Now;
+            DateTime DatumIsteka = DatumIzdavanja.AddMonths(6);
+            Console.WriteLine("Datum isteka: {0}", DatumIsteka);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/13.1_primjer/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._1_primjer
+{
+    public enum RadniDani
+    {
+        Ponedjeljak = 1,
+        Utorak,
+        Srijeda,
+        Cetvrtak,
+        Petak
+    }
+    public enum Ocjene
+    {
+        Nedovoljan = 1,
+        Dovoljan,
+        Dobar,
+        Vrlodobar,
+        Odlican
+    }
+    public enum Katovi
+    {
+        Prizemlje,
+        Prvi,
+        Drugi,
+        Treci,
+        Cetvrti
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Firma algebra = new Firma("Algebra d.o.o.);
+            algebra.Kat = Katovi.Treci;
+        }
+    }
+    class Firma
+    {
+    private Katovi Kat;
+        private string Naziv;
+        
+
+        public Firma(string v)
+        {
+            this.Naziv = v;
+        }
+
+        public Katovi Kat { get => kat; set => Kat = value; }
+
+        public string Naziv { get => naziv; set => naziv = value; }
+
+        public override string ToString()
+        {
+            string kojiKat = "";
+            switch ((int)kat)
+            {
+                case0: kojiKat = "nultom"; break;
+                case1: kojiKat = "prvom"; break;
+                case2: kojiKat = "drugom"; break;
+                case3: kojiKat = "trećem"; break;
+                case4: kojiKat = "četvrtom"; break;
+
+            }
+            return "I naše ime je " + this.naziv + " i nalazimo se na " + kojiKat + ";
+        }
+    }
+}
